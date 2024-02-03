e8ac6a7d898dbeb93efe30a5116b051190e9c208
Kamalakanta Dalai <dalaikamalakanta52@gmail.com>
2024-02-03 19:49:19 +0530

    Finished register & login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import "../style.scss";
 
 const LogIn = (props) => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo"></span>
-        <span className="title"></span>
+        <span className="logo">Welcome Back To Live-Chat</span>
+        <span className="title">Login</span>
         <form action="post">
           <input type="email" name="email" placeholder="Email" />
           <input type="password" name="" placeholder="Password" />
 
           <button>Sign In</button>
-          <p>Don't have an account? Register</p>
         </form>
+        <p>Don't have an account? Register</p>
       </div>
     </div>
   );
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import File from "../assets/exha.jpg";
+import File from "../assets/file.png";
 
 const Register = (props) => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo"></span>
-        <span className="title"></span>
+        <span className="logo">Welcome To Live-Chat</span>
+        <span className="title">Register Now</span>
         <form action="post">
           <input type="text" name="fullName" placeholder="Full Name" />
           <input type="email" name="email" placeholder="Email" />
           <input type="password" name="" placeholder="Password" />
-          <input type="file" id="file" />
+          <input style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
             <img src={File} alt="File Icon" />
+            <span>Add an avatar</span>
           </label>
           <button>Sign Up</button>
-          <p>Already have an account? Login</p>
         </form>
+        <p>Already have an account? Login</p>
       </div>
     </div>
   );
